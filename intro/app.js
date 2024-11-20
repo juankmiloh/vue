250a879df82b54bde2e2ad5fb9fb65a07a5beafa
BitsAmericas <bitsamericas@MacBook-Air-de-BitsAmericas.local>
2024-11-20 12:57:08 -0500

    first commit

---
+++ intro/app.js
@@ -0,0 +1,31 @@
+const { createApp, ref } = Vue;
+
+const app = createApp({
+  // template: `
+  //       <h1>{{ message }}</h1>
+  //       <p>- {{ author }}</p>
+  //   `,
+
+  setup() {
+    const message = ref("I'm Batman");
+    const author = ref("Batman");
+
+    const changeQuote = () => {
+      message.value = "Hola, soy Goku";
+      author.value = "Goku";
+    };
+
+    // setTimeout(() => {
+    //   message.value = "Hola, soy Goku";
+    //   author.value = "Goku";
+    // }, 1000);
+
+    return {
+      message,
+      author,
+      changeQuote,
+    };
+  },
+});
+
+app.mount("#myApp");
