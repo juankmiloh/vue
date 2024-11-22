12b683699aad42f48a2b27acf03297d84f52a1d7
BitsAmericas <bitsamericas@MacBook-Air-de-BitsAmericas.local>
2024-11-21 23:26:09 -0500

    Create UI chat

---
+++ 04-indecision-app/src/components/chat/ChatMessages.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex-1 overflow-y-auto p-4">
+        <div class="flex flex-col space-y-2">
+            <!-- Messages go here -->
+            <ChatBubble />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import ChatBubble from './ChatBubble.vue';
+</script>
