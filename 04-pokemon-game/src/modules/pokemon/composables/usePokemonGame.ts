dee3eef91cba80db1621f5c3df883062bc020945
BitsAmericas <bitsamericas@MacBook-Air-de-BitsAmericas.local>
2024-11-25 19:23:52 -0500

    Propiedades computadas y orden aleatorio en el array

--- 04-pokemon-game/src/modules/pokemon/composables/usePokemonGame.ts
+++ 04-pokemon-game/src/modules/pokemon/composables/usePokemonGame.ts
@@ -1,21 +1,45 @@
-import { onMounted, ref } from 'vue'
-import { GameStatus, type PokemonListResponse } from '../interfaces'
+import { computed, onMounted, ref } from 'vue'
+import { GameStatus, type Pokemon, type PokemonListResponse } from '../interfaces'
 import { pokemonApi } from '../api/pokemonApi'
 
 export const usePokemonGame = () => {
   const gameStatus = ref<GameStatus>(GameStatus.playing)
+  const pokemons = ref<Pokemon[]>([])
+  const pokemonsOptions = ref<Pokemon[]>([])
+  const isLoading = computed(() => pokemons.value.length === 0)
 
-  const getPokemons = async () => {
+  const getPokemons = async (): Promise<Pokemon[]> => {
     const response = await pokemonApi.get<PokemonListResponse>('/?limit=151')
+    const pokemonsArray = response.data.results.map((pokemon) => {
+      const urlParts = pokemon.url.split('/')
+      const id = urlParts.at(-2) ?? 0
+      return {
+        name: pokemon.name,
+        id: +id, // Se coloca el (+) para convertir el resultado a un numero
+      }
+    })
 
-    console.log('response.data', response.data)
+    return pokemonsArray.sort(() => Math.random() - 0.5) // Se ordena el arreglo de manera aleatoria
   }
 
-  onMounted(() => {
-    getPokemons()
+  const getNextOptions = (howMany: number = 4) => {
+    gameStatus.value = GameStatus.playing
+    pokemonsOptions.value = pokemons.value.slice(0, howMany)
+    pokemons.value = pokemons.value.slice(howMany)
+  }
+
+  onMounted(async () => {
+    pokemons.value = await getPokemons()
+    getNextOptions()
+    console.log('pokemonsOptions', pokemonsOptions.value)
   })
 
   return {
     gameStatus,
+    isLoading,
+    pokemonsOptions,
+
+    // Methods
+    getNextOptions,
   }
 }
--- 04-pokemon-game/src/modules/pokemon/pages/PokemonGame.vue
+++ 04-pokemon-game/src/modules/pokemon/pages/PokemonGame.vue
@@ -1,10 +1,10 @@
 <template>
-  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-if="isLoading" class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="text-3xl">Esper por favor</h1>
     <h3 class="animate-pulse">Cargando Pokemons</h3>
   </section>
 
-  <section class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="m-5">¿Quién es este pokemón?</h1>
     <!-- Pokemon Picture -->
     <PokemonPicture />
@@ -18,5 +18,5 @@ import PokemonOptions from '../components/PokemonOptions.vue'
 import PokemonPicture from '../components/PokemonPicture.vue'
 import { usePokemonGame } from '../composables/usePokemonGame'
 
-const { gameStatus } = usePokemonGame()
+const { isLoading } = usePokemonGame()
 </script>
